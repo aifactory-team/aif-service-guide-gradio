524b797260901e76e8f100806e4be1d7dc65c1f8
kimyeonjoon-dev <78746710+kimyeonjoon-dev@users.noreply.github.com>
2023-07-12 12:24:33 +0900

    Update default.py

--- default.py
+++ default.py
@@ -2,17 +2,22 @@ import gradio as gr
 import requests
 import json
 
+# 사용량 확인 API 주소 
 api_request_env = "https://api.aifactory.space/task/checkServiceRequest"
 
+# 사용량 확인 함수 
 def sendRequestForService(key):
   res = requests.post(api_request_env, json= {'service': 'gui', 'key': key})
   return res
 
+# gradio block 생성
 block = gr.Blocks()
 
+# 추론 함수 
 def predict(text, url_params):
   error = ""
   try:      
+    # 키 확인 
     key = url_params['key']   
     # print(key)     
     res = sendRequestForService(key)      
@@ -26,7 +31,8 @@ def predict(text, url_params):
     print("error")
     print(str(e))
   return [text, error, url_params] # 결과 정상인 경우 
-    
+
+#  gradio javascript 함수 
 get_window_url_params = """
     function(text_input, url_params) {
         console.log(text_input, url_params);
@@ -35,7 +41,7 @@ get_window_url_params = """
         return [text_input, url_params];
         }
     """
-
+# block 정의
 with block:
   url_params = gr.JSON({}, visible=True, label="URL Params")
   text_input = gr.Text(label="Input")
@@ -45,6 +51,6 @@ with block:
   btn = gr.Button("Run")
   btn.click(fn=predict, inputs=[text_input, url_params],
             outputs=[text_output, error, url_params], _js=get_window_url_params)
-
+# gradio 시작 
 block.launch(debug=True)
 # block.launch(server_name="0.0.0.0", debug=True)
